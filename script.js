7d5a0a81a8aaaf6c41ba2cd695cf6422431fb7e5
Chewster84 <106880685+Chewster84@users.noreply.github.com>
2022-06-29 07:31:06 -0600

    module4-solutions-assignment

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-var names=new Array();
-names[0]="Yaakov"
-names[1]="John"
-names[2]="jen"
-names[3]="Jason"
-names[4]="Paul"
-names[5]="Frank"
-names[6]="Larry"
-names[7]="Paula"
-names[8]="Laura"
-names[9]="jim"
+//Array
+
+var names = new Array();
+names[0] = "Yaakov"
+names[1] = "John"
+names[2] = "jen"
+names[3] = "Jason"
+names[4] = "Paul"
+names[5] = "Frank"
+names[6] = "Larry"
+names[7] = "Paula"
+names[8] = "Laura"
+names[9] = "jim"
 
 for(var i = 0;i<names.length;i++){
     if(names[i].charAt(0)=== 'J' || names[i].charAt(0)==='j')
